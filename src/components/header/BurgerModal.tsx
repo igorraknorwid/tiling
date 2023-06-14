805bafeb20a59cfb2d39ef42e1af60e6208a4e60
igorraknorwid <igorrakfed@gmail.com>
2023-06-14 12:53:43 +0200

    framer-motion duration

--- src/components/header/BurgerModal.tsx
+++ src/components/header/BurgerModal.tsx
@@ -71,7 +71,7 @@ export default function BurgerModal() {
             initial={{ opacity: 0, y: 500 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: -500 }}
-            transition={{ ease: "easeOut", duration: 0.8 }}
+            transition={{ ease: "easeOut", duration: 0.4 }}
             // animate={{ scale: 1, opacity: 1 }}
             // exit={{ scale: 0.8, opacity: 0 }}
             // transition={{ type: "spring" }}
