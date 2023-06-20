36864f5bb1762749f2ca139de98bdecbc5a217ed
igorraknorwid <igorrakfed@gmail.com>
2023-06-20 13:20:01 +0200

    content

--- src/data/questions.ts
+++ src/data/questions.ts
@@ -7,12 +7,12 @@ export const questions: Question[] = [
     answer:
       "From the first day of demolition to the final walk-through, your bathroom renovation could take a bit less — or substantially more — time than average.$Some bathrooms may take as little as 3-4 weeks to finish, while others take 9-10 weeks, or even longer.",
   },
-  {
-    id: 2,
-    question: "What is the difference between ceramics and porcelain tiles?",
-    answer:
-      "Ceramics and porcelain are both types of ceramic materials, but there are some differences between them.Porcelain is a subtype of ceramic that is more refined and has a higher density than traditional ceramics.$It is fired at a higher temperature, which results in a harder and more durable material.$Porcelain also has a lower water absorption rate compared to ceramics, making it more suitable for areas with high moisture, such as bathrooms or kitchens.",
-  },
+  // {
+  //   id: 2,
+  //   question: "What is the difference between ceramics and porcelain tiles?",
+  //   answer:
+  //     "Ceramics and porcelain are both types of ceramic materials, but there are some differences between them.Porcelain is a subtype of ceramic that is more refined and has a higher density than traditional ceramics.$It is fired at a higher temperature, which results in a harder and more durable material.$Porcelain also has a lower water absorption rate compared to ceramics, making it more suitable for areas with high moisture, such as bathrooms or kitchens.",
+  // },
   {
     id: 3,
     question: "What are the benefits of kitchen tiling?",
@@ -21,15 +21,15 @@ export const questions: Question[] = [
   },
   {
     id: 4,
-    question: "Are ceramic tiles non-slip?",
+    question: "Are tiles non-slip?",
     answer:
-      "Ceramic tiles can have varying degrees of slip resistance, depending on their surface texture and finish. While some ceramic tiles may have a smooth and glossy surface, which can be more slippery, there are also options available that offer increased traction to reduce the risk of slipping.",
+      "Tiles can have varying degrees of slip resistance, depending on their surface texture and finish. While some  tiles may have a smooth and glossy surface, which can be more slippery, there are also options available that offer increased traction to reduce the risk of slipping.",
   },
   {
     id: 5,
-    question: "Is ceramic tile a durable material?",
+    question: "Is  tile a durable material?",
     answer:
-      "Yes, ceramic tile is known for its durability.It is a robust and long-lasting material, making it a popular choice for flooring and wall applications in both residential and commercial settings.$Its resistance to scratches, stains, and moisture, coupled with its ability to withstand heavy foot traffic, makes ceramic tile a durable option for various areas of the home or workplace.",
+      "Yes,  tile is known for its durability.It is a robust and long-lasting material, making it a popular choice for flooring and wall applications in both residential and commercial settings.$Its resistance to scratches, stains, and moisture, coupled with its ability to withstand heavy foot traffic, makes  tile a durable option for various areas of the home or workplace.",
   },
   // Add more questions here...
 ];
