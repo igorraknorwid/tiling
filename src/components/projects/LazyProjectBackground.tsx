3e33cb3f8bcb5a621363a558abbc3fc5c5171166
igorraknorwid <igorrakfed@gmail.com>
2023-06-29 08:22:41 +0200

    about 2

--- src/components/projects/LazyProjectBackground.tsx
+++ src/components/projects/LazyProjectBackground.tsx
@@ -13,7 +13,7 @@ const LazyProjectBackground: FC<Props> = ({ title, location, className }) => {
     const topObserver = new IntersectionObserver((entries) => {
       const entry = entries[0];
       if (entry.isIntersecting) {
-           setIsIntersected(className);
+        setIsIntersected(className);
       }
     });
 
@@ -26,30 +26,38 @@ const LazyProjectBackground: FC<Props> = ({ title, location, className }) => {
     };
   }, []);
 
-
   return (
     <div
       ref={topRef}
-      className={`h-[300px] md:h-[600px] ${isIntersected}  rounded-xl p-5 flex ${isIntersected.length > 0?"justify-end items-end":"justify-center items-center"}`}
+      className={`h-[300px] md:h-[700px] ${isIntersected}  p-5 flex ${
+        isIntersected.length > 0
+          ? "justify-end items-end"
+          : "justify-center items-center"
+      }`}
     >
-      {isIntersected.length === 0 &&
-       <div
-       className="inline-block h-40 w-40 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]"
-       role="status">
-       <span
-         className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-         >L</span>       
-     </div>
-      }
-      {isIntersected.length > 0 &&<div className='m-4'>
-        <div className='bg-[#B86300] text-black text-sm  px-2 py-1 rounded w-max mt-3 font-bold  transition-transform duration-300 hover:scale-125'>
-          <p>See more</p>
+      {isIntersected.length === 0 && (
+        <div
+          className='inline-block h-40 w-40 animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_1.5s_linear_infinite]'
+          role='status'
+        >
+          <span className='!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]'>
+            L
+          </span>
         </div>
-        <div className='md:text-3xl font-bold text-xl text-white'>{title}</div>
-        <div className='md:text-3xl font-bold mt-1 text-xl text-white'>
-          {location}
-        </div>      
-      </div>}
+      )}
+      {isIntersected.length > 0 && (
+        <div className='m-4'>
+          <div className='bg-[#B86300] text-black text-sm  px-2 py-1 rounded w-max mt-3 font-bold  transition-transform duration-300 hover:scale-125'>
+            <p>See more</p>
+          </div>
+          <div className='md:text-3xl font-bold text-xl text-white'>
+            {title}
+          </div>
+          <div className='md:text-3xl font-bold mt-1 text-xl text-white'>
+            {location}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
