87e1ad9a76d517e1e939b565193082376ca21d41
Igor Rak <igorrakfed@gmail.com>
2023-07-26 17:33:11 +0200

    answers

--- src/components/commertial_form/Form.tsx
+++ src/components/commertial_form/Form.tsx
@@ -11,7 +11,7 @@ interface IStep {
     name:string;
     isActive:boolean;
     answers:IAnswer[],
-    isFinish:boolean
+    isFinish?:boolean
 }
 interface IClientData{
     tiling:string
@@ -22,22 +22,49 @@ export const mySteps:IStep[] = [
         name:"tiling",
         isActive:true,     
         answers:[
-            {id:0,text:"Which kind of service you need..."},
+            {id:0,text:"Which kind of service you need?"},
             {id:1,text:"Domestic tiling"},
             {id:2,text:"Comercial tiling"},
         ],
-        isFinish:false,
+      
     },
     {
         id:2,
+        name:"job",
+        isActive:true,     
+        answers:[
+            {id:0,text:"What type of job you need?"},
+            {id:1,text:"New tile instalation/repair"},
+            {id:2,text:"Repair tiles"},
+            {id:3,text:"Reseal tiles"},
+            {id:4,text:"Other tiling job"},
+        ],
+      
+    },
+    {
+        id:3,
+        name:"material",
+        isActive:true,     
+        answers:[
+            {id:0,text:"Do you already have materials?"},
+            {id:1,text:"Now,I expert need help"},
+            {id:2,text:"NYes,I have material"},
+         
+        ],
+      
+    },
+    {
+        id:4,
         name:"term",
         isActive:false,      
         answers:[    
             {id:0,text:"When do you need to start..."},
-            {id:1,text:"As soon as posible"},
-            {id:2,text:"Other"},
-        ],
-        isFinish:true,
+            {id:1,text:"Within 1 month"},
+            {id:2,text:"Within 1-3 months"},
+            {id:3,text:"Within 3-6 month"},
+            {id:4,text:"As soon as posible"},
+            {id:5,text:"Other"},
+        ],       
     }
 ]
 
@@ -73,23 +100,26 @@ const Form: React.FC = () => {
         if (!data.isFinish){
             setStep(s=>s+1)
             console.log(clientData.current)
+            setIsAvalibleAnswer(false)
         } else {
            
         }
     }
     
   return (
-    <section className="bg-white text-black border-4 border-[#B86300] p-4 rounded-lg flex flex-col space-y-2 justify-center">  
-     <label htmlFor="options">Get a Tiling Quote</label>
-     <select id="options" value={answer} onChange={handleInputChange}>       
+    <div className="bg-white text-black border-4 border-[#B86300] p-4 pt-8 rounded-lg flex flex-col space-y-2 justify-center w-full">  
+     <label htmlFor="options" className="bg-[#B86300] text-black font-bold text-xl rounded-2xl text-center py-2 px-4  ">Get a Tiling Quote</label>
+     <div className="flex gap-4">
+     <select className="w-full border border-[#B86300] p-2 py-4" id="options" value={answer} onChange={handleInputChange}>       
         {data?.answers.map((option) => (
-          <option key={option.id} value={option.text} id={option.id.toString()}  >
+          <option className="py-8" key={option.id} value={option.text} id={option.id.toString()}  >
             {option.text}
           </option>
         ))}
       </select>
-     <button onClick={clickHandler} className={`bg-[#B86300] ${!isAvalibleAnswer&&"opacity-50"}`} type="button" disabled={!isAvalibleAnswer}>Next</button>
-    </section>
+      {isAvalibleAnswer&& <button onClick={clickHandler} className={`bg-[#B86300] rounded-xl px-10`} type="button" >Go</button>}
+     </div>       
+    </div>
   );
 };
 
