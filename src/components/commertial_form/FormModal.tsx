daa537879b47425dbac7d48e8e707d8fe1a5e42d
igorraknorwid <igorrakfed@gmail.com>
2023-08-09 10:25:38 +0200

    modal

--- src/components/commertial_form/FormModal.tsx
+++ src/components/commertial_form/FormModal.tsx
@@ -11,7 +11,20 @@ function Modal() {
       {/* <button className='border' onClick={expandClickHandler}>
         Close
       </button> */}
-      <div className='bg-white w-1/2 py-10 text-black rounded-xl'>Modal</div>
+      <div className='bg-white w-1/2 p-10 text-black rounded-xl text-xl'>
+        <h2 className='text-2xl text-center'>
+          Thank you for reaching out to us!{" "}
+        </h2>
+        Your message is important to us, and we're committed to providing you
+        with the assistance you need. Our dedicated team aims to respond to your
+        inquiry <strong>within 48 hours</strong>, either by phone or email,
+        based on the information you've provided in the form.
+        <p>
+          In the meantime, feel free to explore our website for more information
+          about our services. We appreciate your patience and look forward to
+          connecting with you soon.
+        </p>
+      </div>
     </div>
   );
 }
