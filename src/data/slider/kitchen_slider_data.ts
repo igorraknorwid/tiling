9883e71370b03ee7603276d7eac5a78dd92faef1
igorraknorwid <igorrakfed@gmail.com>
2023-09-12 14:54:04 +0200

    slider rwd 2

---
+++ src/data/slider/kitchen_slider_data.ts
@@ -0,0 +1,15 @@
+import { ISliderItem } from "../../types/slider";
+
+import kitchen1 from "../../images/slider/kitchen/1.webp";
+import kitchen2 from "../../images/slider/kitchen/2.webp";
+import kitchen3 from "../../images/slider/kitchen/3.webp";
+import kitchen4 from "../../images/slider/kitchen/4.webp";
+
+const arr: ISliderItem[] = [
+  { id: 1, src: kitchen1, title: "Beautiful Tiling Work" },
+  { id: 2, src: kitchen2, title: "Expert Tiling Services" },
+  { id: 3, src: kitchen3, title: "Quality Tiles for Your Kitchen" },
+  { id: 4, src: kitchen4, title: "Professional Kitchen Solutions" },
+];
+
+export default arr;
